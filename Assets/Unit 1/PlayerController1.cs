2b5e6e8ce92cf2a352123930c32d46d09be2afc9
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2022-04-08 02:42:26 +0300

    prototype 1 update

--- Assets/Unit 1/PlayerController1.cs
+++ Assets/Unit 1/PlayerController1.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerController1 : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    //[SerializeField] private float speed;
+    [SerializeField] private float horsePower;
     [SerializeField] private float turnSpeed;
     private float horizontalInput;
     private float forwardInput;
-
+    [SerializeField] Transform centerOfMass;
+    Rigidbody playerRb;
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        //playerRb.centerOfMass = centerOfMass.position;
+    }
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
     }
 }
